783475a41843078e7486368138ce5e703257fcf9
Prem Anand <probertson@illumina.com>
2020-03-20 13:57:01 +0530

    Commit my Feature Branch

--- MyGitWebApi/Controllers/WeatherControl.cs
+++ MyGitWebApi/Controllers/WeatherControl.cs
@@ -4,3 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
+namespace MyGitWebApi.Controllers
+{
+    public class WeatherControl
+    {
+        
+    }
+}
